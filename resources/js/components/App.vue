d5b15e3a2a4db53006d94c75b0973432cbe68f8a
Paul Poirel <poirelpa@gmail.com>
2021-08-18 00:15:03 +0200

    premiers pas avec vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="two-way-binding">
+    <p>{{ message }}</p>
+    <input v-model="message" />
+    <button @click="changeMessage">changer</button>
+  </div>
+</template>
+
+
+<script>
+    export default {
+      data() {
+        return {
+          message: "test"
+        }
+      },
+        methods : {
+            changeMessage () {
+                this.message = "tata";
+            }
+        }
+    }
+</script>
